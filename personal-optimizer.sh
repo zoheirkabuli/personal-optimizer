244c14c9a85467d186fabeadbc52eb26bdf30c7d
Zoheir Kabuli <zoheyrk@gmail.com>
2024-05-06 13:32:36 +0330

    add download caddy.json to caddy_prerequisites

--- personal-optimizer.sh
+++ personal-optimizer.sh
@@ -79,6 +79,7 @@ caddy_prerequisites(){
   sudo mkdir /home/tls
   sudo mkdir -p /var/www/html
   sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl && curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg && curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list && sudo apt update && sudo apt install caddy
+  sudo wget "https://raw.githubusercontent.com/zoheirkabuli/personal-optimizer/main/caddy.json"
 
   echo
   green_msg 'Caddy prerequisites installed.'
